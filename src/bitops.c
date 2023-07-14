1257f07c08c413f63896d178a2abc29e5dbc21b9
ocsmit <osmith2@ncsu.edu>
2023-07-14 15:20:07 -0400

    Move operations to bit operators

--- src/bitops.c
+++ src/bitops.c
@@ -16,59 +16,35 @@ unsigned int BitArray_bitread(BitArray* bit_arr, unsigned int j) {
   unsigned int r = sig_bit_idx(j, bit_arr->size);
 
   // TODO: Add range checking
-  return (bit_arr->v[j/bit_arr->size] / (1 << (bit_arr->size - r))) % 2;
+  return (bit_arr->v[j/bit_arr->size] >> (j % bit_arr->size)) & 1;
 }
 
 
 void BitArray_bitset(BitArray* bit_arr, unsigned int j)
 {
-  // idx of bit j in word of size w
-  unsigned int r, idx, word;
-  r = sig_bit_idx(j, bit_arr->size); 
-  idx = j/bit_arr->size;
-  word = bit_arr->v[idx];
-
-
-  if ((word / (1 << (bit_arr->size - r))) % 2 == 0) {
-    bit_arr->v[idx] = word + (1 << (bit_arr->size - r));
-  }
+  bit_arr->v[j/bit_arr->size] |= 1 << (j % bit_arr->size);
 }
 
 void BitArray_bitclear(BitArray* bit_arr, unsigned int j)
 {
-
-  // idx of bit j in word of size w
-  unsigned int r, idx, word;
-  r = sig_bit_idx(j, bit_arr->size); 
-  idx = j/bit_arr->size;
-  word = bit_arr->v[idx];
-
-  if ((word / (1 << (bit_arr->size - r))) % 2 == 1) {
-    bit_arr->v[idx] = word + (1 << (bit_arr->size - r));
-  }
+  bit_arr->v[j/bit_arr->size] &= ~(1 << (j % bit_arr->size));
 }
 
 int BitArray_bitsread(BitArray* bit_arr, unsigned int j1, unsigned int j)
 {
   if (j1 > j) return 0;
 
-  // idx of j
-  unsigned int r = sig_bit_idx(j, bit_arr->size);
-
-  // Early return if bits contained in same element in W
   if (j1 / bit_arr->size == j / bit_arr->size) {
-    return (int)(
-      (bit_arr->v[j/bit_arr->size] / (1 << (bit_arr->size - r))) % (1 << (j - j1 + 1))
-    );
+    return 
+      (bit_arr->v[j/bit_arr->size] >> (j1 % bit_arr->size)) & ((1 << (j-j1+1)) - 1);
   }
 
-  // idx of j'
-  unsigned int r1= sig_bit_idx(j1, bit_arr->size) - 1;
-  return (int)(
-    (bit_arr->v[j/bit_arr->size] / (1 << (bit_arr->size - r))) +
-    (bit_arr->v[j1/bit_arr->size] % (1 << (bit_arr->size - r1))) *
-    (1 << r)
-  );
+  return (
+  (bit_arr->v[j1/bit_arr->size] >> (j1 % bit_arr->size)) |
+    (bit_arr->v[j/bit_arr->size] & 
+    ((1 << ((j+1) % bit_arr->size)) - 1)) <<
+    (bit_arr->size - (j1 % bit_arr->size))
+);
 }
 
 int BitArray_read(BitArray* bit_arr, unsigned int i)
