bfb87be686c2211f62b1e1e8050d0e0b787fc2f1
ocsmit <osmith2@ncsu.edu>
2023-07-17 14:11:38 -0400

    Update header

--- src/bitarr.h
+++ src/bitarr.h
@@ -5,9 +5,10 @@
  * Compact bit arrays are space effecient representation of the classical array
  * as described in "Compact Data Structures, a practical approach" (https://doi.org/10.1017/CBO9781316588284).
  *
- * Arrays are stored in a compact form by using only the bites needed for each
+ * Arrays are stored in a compact form by using only the bits needed for each
  * element. E.g. below the array A is shown to be stored using only two uint32
- * elements (W).
+ * elements (W). From the compact representation, the values from the original
+ * array (A) can be read, written, and operated on.
  *
  *     ┌──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┐
  *  A  │  20  │  18  │  22  │  22  │  16  │  21  │  11  │  22  │  21  │  21  │
